4f3663454d3620846fc3b7354e4761b47fd5271c
mzac <zachary.mcgibbon@gmail.com>
2016-01-28 20:41:46 -0500

    Update gps_check.sh

--- gps_check.sh
+++ gps_check.sh
@@ -33,39 +33,39 @@ fi
 # Start the loop
 while true
 do
-  # Get GPS Data in JSON format from gpsd
-  tpv=$($gpspipe_bin -w -n 5 | grep -m 1 TPV | python -mjson.tool)
-  gps_date=$(echo "$tpv" | grep "time" | cut -d: -f2 | cut -dT -f1 | cut -d, -f1 | tr -d ' ' | tr -d '"')
-  gps_time=$(echo "$tpv" | grep "time" | cut -dT -f2 | cut -d. -f1 | cut -d, -f1 | tr -d ' ')
-  lon=$(echo "$tpv" | grep "lon" | cut -d: -f2 | cut -d, -f1 | tr -d ' ')
-  lat=$(echo "$tpv" | grep "lat" | cut -d: -f2 | cut -d, -f1 | tr -d ' ')
-  alt=$(echo "$tpv" | grep "alt" | cut -d: -f2 | cut -d, -f1 | tr -d ' ' | awk '{print int($1)}')
-  spd=$(echo "$tpv" | grep "speed" | cut -d: -f2 | cut -d, -f1 | tr -d ' ')
-  track=$(echo "$tpv" | grep "track" | cut -d: -f2 | cut -d, -f1 | tr -d ' ' | awk '{print int($1)}')
+        # Get GPS Data in JSON format from gpsd
+        tpv=$($gpspipe_bin -w -n 5 | grep -m 1 TPV | python -mjson.tool)
+        gps_date=$(echo "$tpv" | grep "time" | cut -d: -f2 | cut -dT -f1 | cut -d, -f1 | tr -d ' ' | tr -d '"')
+        gps_time=$(echo "$tpv" | grep "time" | cut -dT -f2 | cut -d. -f1 | cut -d, -f1 | tr -d ' ')
+        lon=$(echo "$tpv" | grep "lon" | cut -d: -f2 | cut -d, -f1 | tr -d ' ')
+        lat=$(echo "$tpv" | grep "lat" | cut -d: -f2 | cut -d, -f1 | tr -d ' ')
+        alt=$(echo "$tpv" | grep "alt" | cut -d: -f2 | cut -d, -f1 | tr -d ' ' | awk '{print int($1)}')
+        spd=$(echo "$tpv" | grep "speed" | cut -d: -f2 | cut -d, -f1 | tr -d ' ')
+        track=$(echo "$tpv" | grep "track" | cut -d: -f2 | cut -d, -f1 | tr -d ' ' | awk '{print int($1)}')
 
-  # Convert speed from meters per second to kilometers per hour
-  spd=`echo $spd | awk '{print int($1 * 3.6)}'`
+        # Convert speed from meters per second to kilometers per hour
+        spd=`echo $spd | awk '{print int($1 * 3.6)}'`
 
-  # Print GPS Results
-  echo -e "Date:\t\t$gps_date"
-  echo -e "Time:\t\t$gps_time"
-  echo -e "Longitude:\t$lon"
-  echo -e "Latitude:\t$lat"
-  echo -e "Altitude:\t$alt"
-  echo -e "Speed:\t\t$spd"
-  echo -e "Track:\t\t$track"
+        # Print GPS Results
+        echo -e "Date:\t\t$gps_date"
+        echo -e "Time:\t\t$gps_time"
+        echo -e "Longitude:\t$lon"
+        echo -e "Latitude:\t$lat"
+        echo -e "Altitude:\t$alt"
+        echo -e "Speed:\t\t$spd"
+        echo -e "Track:\t\t$track"
 
-  echo -e "Sleeping for $update_interval seconds...\n"
-  sleep $update_interval
-  echo "--------------------------------------------------------------------------------"
+        echo -e "Sleeping for $update_interval seconds..."
+        echo "--------------------------------------------------------------------------------"
+        sleep $update_interval
 
-  # Clear all vars
-  unset tpv
-  unset gps_date
-  unset gps_time
-  unset lat
-  unset lon
-  unset alt
-  unset spd
-  unset track
+        # Clear all vars
+        unset tpv
+        unset gps_date
+        unset gps_time
+        unset lat
+        unset lon
+        unset alt
+        unset spd
+        unset track
 done
