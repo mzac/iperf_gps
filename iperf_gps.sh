e469c25db618f281da9bd6dc707f2f83bf987db9
mzac <zachary.mcgibbon@gmail.com>
2016-02-02 13:51:26 -0500

    Update iperf_gps.sh

--- iperf_gps.sh
+++ iperf_gps.sh
@@ -12,10 +12,10 @@ usage() {
         echo -e "$0 -i [server_ip]\n"
         echo -e "Required:\n"
         echo -e "-i [server_ip]\t\tIP Address of the iPerf Server"
-        echo -e "Optional:\n"
+        echo -e "\nOptional:\n"
         echo -e "-m\t\t\tRun through tests manually (no sleep)"
-        echo -e "-p\t\t\tiPerf port to connect to (default is 5001)"
-        echo -e "-s\t\t\tSleep interval between tests (default is 10 seconds)"
+        echo -e "-p [port]\t\tiPerf port to connect to (default is 5001)"
+        echo -e "-s [seconds]\tSleep interval between tests (default is 10 seconds)"
         echo -e "-u\t\t\tRun iPerf with UDP tests (default is TCP)"
         echo -e "-w [base_filename]\tText that will be included in the filename\n"
         exit 0
