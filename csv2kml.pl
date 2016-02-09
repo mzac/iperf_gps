16e4bbd746b449c8764e95cad824fe7b485d9a40
mzac <zachary.mcgibbon@gmail.com>
2016-02-09 16:10:25 -0500

    Update csv2kml.pl

--- csv2kml.pl
+++ csv2kml.pl
@@ -1,1 +1,24 @@
+#!/usr/bin/perl
 
+use Getopt::Long;
+use Text::CSV;
+
+check_options();
+
+sub usage {
+        print "\nSyntax:\n";
+        print "$0 -k <csv_file>\n\n";
+}
+
+sub check_options {
+        Getopt::Long::Configure ("bundling");
+        GetOptions(
+                'h'     => \$o_help,    'help'          => \$o_help,
+                'c:s'   => \$o_csv,     'csv:s'         => \$o_csv,
+        );
+
+        if (defined($o_help)) {
+                usage();
+                exit;
+        }
+}
